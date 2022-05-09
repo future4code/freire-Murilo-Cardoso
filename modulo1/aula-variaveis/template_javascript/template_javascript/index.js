c4438a42757593ac4ed02eff2ce9c50da430acbc
Murilo Cardoso <murilo_mac@hotmail.com>
2022-05-09 15:48:01 -0300

    enviando o arquivo da aula-variaveis

---
+++ modulo1/aula-variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,40 @@
+//1- 10, 5
+//2- 10,10,10
+//3- horas, salarioHoras
+
+//const nome 
+//let idade
+//console.log(typeof nome, idade)
+
+//o resultado deu um erro pois não tem nada atribuido as variaveis
+
+//const nome = prompt("qual o neu nome?")
+//let idade = prompt("qual a sua idade?")
+//console.log(typeof nome, idade)
+//houve um erro pois estava com as variaveis declaradas nas linhas/numerações acima 
+//então comentei para ignorar o código e abri novas variaveis com prompt para perguntar
+//console.log("Olá", nome, "você tem", idade);
+
+//let roupaCor = ("Você está usando roupa azul?")
+//let meiaLonga = ("Você está usando meia longa?")
+//let camisetaCurta = ("Você está usando camiseta curta?")
+
+//let resRoupa = ("sim")
+//let resMeiaLonga = ("não")
+//let resCamisetaCurta = ("não")
+
+//console.log(roupaCor, resRoupa)
+//console.log(meiaLonga, resMeiaLonga)
+//console.log(camisetaCurta, resCamisetaCurta)
+
+
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+

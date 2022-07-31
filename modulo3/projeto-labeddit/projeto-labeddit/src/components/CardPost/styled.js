ffa174ef12104b2e71cf4cf9c249c207bf5f7e26
Murilo Cardoso <murilo_mac@hotmail.com>
2022-07-31 18:17:08 -0300

    ok login ok cadastro

---
+++ modulo3/projeto-labeddit/projeto-labeddit/src/components/CardPost/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const CardMed = styled.div`
+display: flex;
+justify-content: center;
+width: 250px;
+height: 250px;
+border: 4px solid black;
+text-align: center;
+margin: 5px;
+`
--- modulo3/projeto-labeddit/projeto-labeddit/src/pages/LoginPage/styled.js
+++ modulo3/projeto-labeddit/projeto-labeddit/src/pages/LoginPage/styled.js
@@ -4,8 +4,7 @@ export const DivButtonLogin = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    border: 3px solid pink;
-    justify-content: space-between
+    justify-content: space-between;
 
 `
 export const ScreenContainerLogin = styled.div`
@@ -13,7 +12,6 @@ export const ScreenContainerLogin = styled.div`
     flex-direction: column;
     align-items: center;
     margin-top: 15px;
-    border: 2px solid green;
     height: 500px;
     margin-top: 10vh;
     padding-top: 5vh;
@@ -33,6 +31,5 @@ export const InputsContainer = styled.div`
     max-width: 450px;
     max-height: 450px;
     align-items: center;
-    border: solid black 2px;
     
 `

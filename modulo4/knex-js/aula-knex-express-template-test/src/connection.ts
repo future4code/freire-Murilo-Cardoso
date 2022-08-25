cc018bad105c2958deffcc97faec21f76a905020
unknown <murilo_mac@hotmail.com>
2022-08-25 09:42:42 -0300

    deletando arquivos

--- modulo4/knex-js/aula-knex-express-template-test/src/connection.ts
+++ modulo4/knex-js/aula-knex-express-template-test/src/connection.ts
@@ -3,11 +3,11 @@ import knex from "knex";
  const connection = knex({
     client: "mysql",
     connection: {
-        host:"35.226.146.116",
-        port:3306,
-        user:"freire-murilo-cardoso",
-        password:"a3ApcmTtPiU8dqefiEao",
-        database:"freire-murilo-cardoso",
+        host:"",
+        port:,
+        user:"",
+        password:"",
+        database:"",
         multipleStatements:true
 
     }

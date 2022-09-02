b92ea8879cc7268d495b293fb6c90ec94cadb2b5
unknown <murilo_mac@hotmail.com>
2022-09-02 14:45:54 -0300

    craicao endpoint products

--- modulo4/labecommerce/labecommerce/src/index.ts
+++ modulo4/labecommerce/labecommerce/src/index.ts
@@ -1,15 +1,15 @@
 import { AddressInfo } from "net";
 import app from "./app";
 import { createUser } from "./endpoints/createUser";
+import createProducts from "./endpoints/createProducts"
 
 
 //ENDPOINTS 
 
 app.post("/user", createUser)
+app.post("/products", createProducts)
 
-/* app.get("/endereco/:cep", getAddress)
-
-app.get("/list", getAllAddress) */
+//app.get("/users", getUsers)
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
---
+++ modulo4/labecommerce/labecommerce/src/types/typesProducts.ts
@@ -0,0 +1,13 @@
+
+export type ProductsInsert = {
+    name: string,
+    price: number,
+    image_url: string
+}
+
+export type ProductsData = {
+    id: string,
+    name: string,
+    price: number,
+    image_url: string
+}
